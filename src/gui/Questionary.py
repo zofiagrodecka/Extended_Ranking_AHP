473ac802742358913fed58e91760443ab6215091
Pseudo-Swetoniusz <jbtokarska@gmail.com>
2022-01-12 15:18:01 +0100

    Added new gui file

---
+++ src/gui/Questionary.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QLabel, QWidget, QMainWindow, QHBoxLayout, QVBoxLayout, QPushButton, \
+    QFileDialog, QLineEdit, QSlider, QGridLayout, QRadioButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+import csv
+import numpy
+from src.app.AHPCalculator import AHPCalculator
+from copy import deepcopy
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_template import FigureCanvas
+from matplotlib.figure import Figure
+
+class Questionary(QWidget):
+    def __init__(self):
+        super(Questionary, self).__init__()
+        self.criteria_number = 0
+        self.alternative_number = 0
+        self.criteria = []
+        self.subcriteria = []
+        self.allcriteria = []
+        self.alternatives = []
+        self.initWindow()
+
+    def initWindow(self):
+        self.setGeometry(200, 200, 500, 230)
+        self.setWindowTitle("AHP-Questionary")
+        self.setWindowIcon(QIcon('pathology.png'))
+        self.setStyleSheet("background-color:#b8b8b8")
+        self.title = QLabel(self)
+        self.title.setText("Prosta aplikacja licząca ranking AHP")
+        self.show()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from src.gui.MainWindow import GUIWindow
+from src.gui.Questionary import Questionary
 
 
 def main(arg):
     app = QApplication(arg)
     gui = GUIWindow()
+    quest = Questionary()
     sys.exit(app.exec_())
 
 
